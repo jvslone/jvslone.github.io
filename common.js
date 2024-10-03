295137f6daebb06724c35ee7ee960a43c9784a2a
Jim <jvslone@wm.edu>
2024-10-03 05:13:53 -0400

    Test

---
+++ common.js
@@ -0,0 +1,32 @@
+// Define the watermark string
+const watermark = "&copy; MIT License.";
+
+// Define the navigation bar HTML
+const navbar = `
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="test.html">Test</a>
+    </nav>
+`;
+
+// Function to insert the watermark in the footer
+function insertWatermark() {
+    const footer = document.querySelector('footer');
+    if (footer) {
+        footer.innerHTML = watermark;
+    }
+}
+
+// Function to insert the navigation bar
+function insertNavbar() {
+    const body = document.querySelector('header');
+    if (body) {
+        body.insertAdjacentHTML('afterend', navbar);
+    }
+}
+
+// Run the functions on page load
+window.onload = function() {
+    insertNavbar();
+    insertWatermark();
+};

25b39312dcf976059a9c4401f8a29c99ce31c44e
Jim <jvslone@wm.edu>
2024-11-10 14:18:28 -0500

    Lame

--- test3sim3_script.js
+++ test3sim3_script.js
@@ -139,10 +139,14 @@ function startSimulation3() {
         this.y += vector.y * this.speed;
 
         // Wrap around edges
-        if (this.x < 0) this.x = canvas.width;
-        if (this.x > canvas.width) this.x = 0;
-        if (this.y < 0) this.y = canvas.height;
-        if (this.y > canvas.height) this.y = 0;
+        // if (this.x < 0) this.x = canvas.width;
+        // if (this.x > canvas.width) this.x = 0;
+        // if (this.y < 0) this.y = canvas.height;
+        // if (this.y > canvas.height) this.y = 0;
+        if (this.x < 0) this.reset();
+        if (this.x > canvas.width) this.reset();
+        if (this.y < 0) this.reset();
+        if (this.y > canvas.height) this.reset();
       } else {
         this.reset();
       }
